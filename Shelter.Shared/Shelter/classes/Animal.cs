889c04026ee5d0b78711f7a84a4d0f147c35b5c8
coldoem <lolfail69xd@gmail.com>
2019-10-02 17:21:13 +0200

    animal class

--- Shelter.Shared/Shelter/classes/Animal.cs
+++ Shelter.Shared/Shelter/classes/Animal.cs
@@ -4,6 +4,19 @@ namespace Shelter
 {
     public abstract class Animal
     {
-        
+        public String Name;
+        public String DateOfBirth;
+        public boolean IsChecked;
+        public boolean KidFriendly;
+        public String DateOfArrival;
+
+        public Animal(String Name, String DateOfBirth, boolean IsChecked, boolean KidFriendly, String DateOfArrival){
+            this.Name = Name;
+            this.DateOfBirth = DateOfBirth;
+            this.IsChecked = IsChecked;
+            this.KidFriendly = KidFriendly;
+            this.Since = DateOfArrival;
+        }
+
     }
 }
