8b3f0d0a671bb0090c68e24acfcc6971cdba2b3c
NicolasV <vanhuynegem.n@hotmail.com>
2019-10-02 17:05:26 +0200

    added classes + moving classes

---
+++ Shelter.Shared/Shelter/classes/Dragon.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Shelter
+{
+    public class Dragon : Animal
+    {
+        
+    }
+}
---
+++ Shelter.Shared/Shelter/classes/Panda.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Shelter
+{
+    public class Panda : Animal
+    {
+        
+    }
+}
