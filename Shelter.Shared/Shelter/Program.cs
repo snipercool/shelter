d2e6ca2391bc3c99b9861b54b02d180fb067679e
coldoem <lolfail69xd@gmail.com>
2019-10-02 16:40:26 +0200

    starbestanden

---
+++ Shelter.Shared/Shelter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shelter
+{
+    public abstract class Animals{
+
+    }
+    public abstract class Employees{
+        
+    }
+    public class Dog : Animals{
+
+    }
+    public class Cat : Animals{
+
+    }
+    public class Manager : Employees{
+
+    }
+    public class Caretaker : Employees{
+
+    }
+    public class Administrator : Employees{
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
