ffaaacaa818960960daccd50c6fd17e897c081dd
tikoStudio <tikoStudio@github.com>
2019-10-04 10:03:13 +0200

    filled in caretaker,admin, manager

--- Shelter.Shared/Shelter/classes/Manager.cs
+++ Shelter.Shared/Shelter/classes/Manager.cs
@@ -4,6 +4,7 @@ namespace Shelter
 {
     public class Manager : Employee
     {
-        
+        public Manager(string Name, string DateOfBirth, string Gender, string Mail)
+        : base(Name, DateOfBirth, Gender, Mail){}
     }
 }
