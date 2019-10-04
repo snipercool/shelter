56bfd3ca572f1204692f762f7b1a4b255b2483e9
tikoStudio <tikoStudio@github.com>
2019-10-04 09:51:45 +0200

    employee fixing

--- Shelter.Shared/Shelter/classes/Employee.cs
+++ Shelter.Shared/Shelter/classes/Employee.cs
@@ -4,6 +4,17 @@ namespace Shelter
 {
     public abstract class Employee
     {
-        
+       public Employee(string name, string DateOfBirth, string Gender, string Mail){
+            this.Name = Name;
+            this.DateOfBirth = DateOfBirth;
+            this.Gender = Gender;
+            this.Mail = Mail;
+    } 
+    public string Name { get; set; }
+    public string DateOfBirth { get; set; }
+    public string Gender { get; set;}
+    public string Mail { get; set;}
+
+
     }
 }
