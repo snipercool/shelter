0fddcb58e834b6ac4925d08bcc69ee62c5d98041
tikoStudio <tikoStudio@github.com>
2019-10-09 16:37:58 +0200

    katjes uwu

--- newMVCproject/Controllers/ShelterController.cs
+++ newMVCproject/Controllers/ShelterController.cs
@@ -8,7 +8,9 @@ namespace newMVCproject.Controllers
         public IActionResult Index()
         {   
             var cats = new List<Cat>{};
-            cats.add(newCat);
+            cats.Add(new Cat("Felix","19/01/2005",true, true, "23/09/2019"));
+            cats.Add(new Cat("Whiskers","22/07/2008",true, false, "12/06/2018"));
+            cats.Add(new Cat("Misty","01/01/2001",true, true, "12/06/2009"));
             ViewData["Message"]="Hello, This is my view";
             return View();
         }
