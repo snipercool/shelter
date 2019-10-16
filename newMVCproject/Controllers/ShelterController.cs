de656ae94108d7c13fa9782638f7ab58f9b1f52c
coldoem <lolfail69xd@gmail.com>
2019-10-16 16:44:01 +0200

    pass cat id through shelter controller

--- newMVCproject/Controllers/ShelterController.cs
+++ newMVCproject/Controllers/ShelterController.cs
@@ -2,22 +2,42 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Shelter;
 using newMVCproject.Models;
+using System.Linq;
 
 namespace newMVCproject.Controllers
 {
     public class ShelterController:Controller
     {
+        public List<Cat> GetCats(){
+            var cats = new List<Cat>{};
+            cats.Add(new Cat(0,"Felix","19/01/2005",true, true, "23/09/2019"));
+            cats.Add(new Cat(1,"Bacardi","22/07/2008",true, false, "12/06/2018"));
+            cats.Add(new Cat(2,"Misty","01/01/2001",true, true, "12/06/2009"));
+            return cats;
+        }
+
         public IActionResult Overview()
         {   
-            var cats = new List<Cat>{};
-            cats.Add(new Cat("Felix","19/01/2005",true, true, "23/09/2019"));
-            cats.Add(new Cat("Bacardi","22/07/2008",true, false, "12/06/2018"));
-            cats.Add(new Cat("Misty","01/01/2001",true, true, "12/06/2009"));
-            return View(new ListViewModel { cats = cats});
+            var cats = GetCats();
+            return View(new ListViewModel { cats = cats });
         }
-        public IActionResult Details()
+
+        public Cat getTargetCat(List<Cat> cats, int id){
+            for(int i = 0; i < cats.Count; i++){
+                if(cats[i].Id == id){
+                    Cat targetCat = cats[i];
+                    return targetCat;
+                }
+            }
+            return null;
+        }
+
+        public IActionResult Details(int id)
         {
-            return View(new ListViewModel());
+            var cats = GetCats();
+            var targetCat = getTargetCat(cats, id);
+
+            return View(targetCat);
         }
     }
 }
