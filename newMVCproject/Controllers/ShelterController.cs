446e9a3acccbc85c4e1cded5187effa5b6ad1d58
coldoem <lolfail69xd@gmail.com>
2019-11-12 11:36:26 +0100

    created database, adjusted Controllers

--- newMVCproject/Controllers/ShelterController.cs
+++ newMVCproject/Controllers/ShelterController.cs
@@ -8,43 +8,22 @@ namespace newMVCproject.Controllers
 {
     public class ShelterController:Controller
     {
-
-       public List<Cat> GetCats(){
-            var cats = new List<Cat>{};
-            cats.Add(new Cat(0,"Felix","19/01/2005",true, true, "23/09/2019"));
-            cats.Add(new Cat(1,"Bacardi","22/07/2008",true, false, "12/06/2018"));
-            cats.Add(new Cat(2,"Misty","01/01/2001",true, true, "12/06/2009"));
-            return cats;
-        }
-
         public IActionResult Overview()
         {   
             var cats = GetCats();
             return View(new ListViewModel { cats = cats });
         }
 
-        public Cat getTargetCat(List<Cat> cats, int id){
-            for(int i = 0; i < cats.Count; i++){
-                if(cats[i].Id == id){
-                    Cat targetCat = cats[i];
-                    return targetCat;
-                }
-            }
-            return null;
-        }
-
         public IActionResult Details(int id)
         {
-            var cats = GetCats();
-            var targetCat = getTargetCat(cats, id);
+            var targetCat = AnimalDatabase.Shelter.Cats.FirstOrDefault(x => x.Id == id);
 
             return View(targetCat);
         }
 
         public IActionResult Delete(int id)
         {
-            var cats = GetCats();
-            var targetCat = getTargetCat(cats,id);
+            var targetCat = AnimalDatabase.Shelter.Cats.FirstOrDefault(x => x.Id == id);
             
             return View(targetCat);
         }
@@ -52,8 +31,7 @@ namespace newMVCproject.Controllers
         [HttpPost]
         public IActionResult DoDelete(int id)
         {
-            var cats = GetCats();
-            var target = cats.FirstOrDefault(x => x.Id == id);
+            var target = AnimalDatabase.Shelter.Cats.FirstOrDefault(x => x.Id == id);
             if (target == default(Cat))
             {
                 return NotFound();
@@ -66,16 +44,14 @@ namespace newMVCproject.Controllers
 
         public IActionResult Edit(int id)
         {
-            var cats = GetCats();
-            var targetcat = getTargetCat(cats, id);
+            var targetcat = AnimalDatabase.Shelter.Cats.FirstOrDefault(x => x.Id == id);
 
             return View(targetcat);
         }
 
         public IActionResult DoEdit(int id, string name)
         {
-            var cats = GetCats();
-            var targetCat = getTargetCat(cats,id);
+            var targetCat = AnimalDatabase.Shelter.Cats.FirstOrDefault(x => x.Id == id);
             /*  doesn't work
             targetCat.Name = name;
             */
---
+++ newMVCproject/Models/AnimalDatabase.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Shelter.Shared;
+
+namespace newMVCproject.Models
+{
+    public class AnimalDatabase
+    {
+        private static bool _isInitialized = false;
+        private static Shelter.Shared.Shelter _shelter = null;
+        
+        private static void Initialize(){
+            if (!_isInitialized){
+
+                var shelter = new Shelter.Shared.Shelter(){
+                    var Cats = new List<Cat> {
+                        new Cat {Id = 0, Name = "Felix", DateOfBirth = "19/01/2005", IsChecked = true, KidFriendly = true, DateOfArrival = "23/09/2019"},
+                        new Cat {Id = 1, Name = "Bacardi", DateOfBirth = "22/07/2008", IsChecked = true, KidFriendly = false, DateOfArrival = "12/06/2018"},
+                        new Cat {Id = 2, Name = "Misty", DateOfBirth = "01/01/2001", IsChecked = true, KidFriendly = true, DateOfArrival = "12/06/2009"}
+                    }
+                };
+
+                _shelter = shelter;
+                _isInitialized = true;
+            }
+        }
+
+        public static Shared.Shelter Shelter
+        {
+            get{
+                Initialize();
+                return _shelter;
+            }
+        }
+    }
+}
