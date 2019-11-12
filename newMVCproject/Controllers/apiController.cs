c2eb47fe08d9a64092722956f3c2b43b134b5053
coldoem <lolfail69xd@gmail.com>
2019-11-12 13:49:36 +0100

    instructions unclear, got head stuck in wall

--- newMVCproject/Controllers/apiController.cs
+++ newMVCproject/Controllers/apiController.cs
@@ -7,16 +7,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using newMVCproject.Models;
 using Shelter.Shared;
+using System.Net.Http.Headers;
 
 namespace newMVCproject.Controllers
 {
-    public class apiController : Controller
+    public class apiController : ControllerBase 
     {
         [Route("api/shelters")]
-        //
+        
 
         [Route("api/shelters/<id>/animals")]
         //
-        
+
     }
 }
