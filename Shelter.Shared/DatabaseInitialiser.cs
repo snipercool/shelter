684d3edc6373a4d6e3b711bcbce47ea8eb791e46
coldoem <lolfail69xd@gmail.com>
2019-11-13 17:11:19 +0100

    shelter update

--- Shelter.Shared/DatabaseInitialiser.cs
+++ Shelter.Shared/DatabaseInitialiser.cs
@@ -54,11 +54,14 @@ namespace Shelter.Shared
       _context.Breweries.Add(brewery);
 
       _context.SaveChanges(); */
-
-        Cats = new List<Cat> {
-            new Cat {Id = 1, Name = "Felix", DateOfBirth = "19/01/2005", IsChecked = true, KidFriendly = true, DateOfArrival = "23/09/2019"},
-            new Cat {Id = 2, Name = "Bacardi", DateOfBirth = "22/07/2008", IsChecked = true, KidFriendly = false, DateOfArrival = "12/06/2018"},
-            new Cat {Id = 3, Name = "Misty", DateOfBirth = "01/01/2001", IsChecked = true, KidFriendly = true, DateOfArrival = "12/06/2009"}
+        Var shelter = new Shelter(){
+            Cats = new List<Cat> {
+                new Cat {Id = 1, Name = "Felix", DateOfBirth = "19/01/2005", IsChecked = true, KidFriendly = true, DateOfArrival = "23/09/2019"},
+                new Cat {Id = 2, Name = "Bacardi", DateOfBirth = "22/07/2008", IsChecked = true, KidFriendly = false, DateOfArrival = "12/06/2018"},
+                new Cat {Id = 3, Name = "Misty", DateOfBirth = "01/01/2001", IsChecked = true, KidFriendly = true, DateOfArrival = "12/06/2009"}
+            }
         }
+        _context.Shelters.Add(shelter);
+        _context.SaveChanges();
     }
 }
--- Shelter.Shared/Shelter.cs
+++ Shelter.Shared/Shelter.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 
 namespace Shelter.Shared
 {
-  public class Shelter
-    {
-        public List<Cat> Cats { get; set; }
-    }
+  public class Shelter : BaseDbClass
+  {
+    public string name { get; set; }
+    public List<Animal> Animals { get; set; }
+    public List<Employee> Employees { get; set; }
+  }
 }
