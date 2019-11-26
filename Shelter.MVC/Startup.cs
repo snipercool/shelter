1f30adc83e5ad57c48e153af22101be90671b6c1
NicolasV <vanhuynegem.n@hotmail.com>
2019-11-26 16:48:53 +0100

    debugging

--- Shelter.MVC/Startup.cs
+++ Shelter.MVC/Startup.cs
@@ -63,7 +63,7 @@ namespace Shelter.MVC
 
              services.AddMvc();
 
-
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDatabaseInitializer databaseInitializer)
         {
