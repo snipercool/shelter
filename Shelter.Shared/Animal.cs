a9250385db8ed403968fc0a7e3c974928c239abd
Moophie <michael.van.lierde@hotmail.com>
2020-01-10 18:50:46 +0100

    Made shelterId required

--- Shelter.Shared/Animal.cs
+++ Shelter.Shared/Animal.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shelter.Shared
 {
     public abstract class Animal : BaseDbClass
@@ -7,6 +10,8 @@ namespace Shelter.Shared
         public bool IsChecked { get; set; }
         public bool KidFriendly { get; set; }
         public string DateOfArrival{ get; set; }
+        
+        [Required]
         public int ShelterId { get; set; }
     }
 }
